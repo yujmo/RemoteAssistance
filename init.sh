680f5854b03e75abfeea3f4930c2812d2fd9f102
yujmo <yujmo@users.noreply.github.com>
2018-09-23 12:04:21 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-touch /root/test_`date +%Y-%m-%d-%H:%M:%S`
-#autossh -M 7281 -fCNR 7280:localhost:22 root@openfaas.cn
+echo "system test" > /root/test_`date +%Y-%m-%d-%H:%M:%S`.log
+
+autossh -M 7281 -fCNR 7280:127.0.0.1:22 root@openfaas.cn
+ssh  -CqTfnN -R 0.0.0.0:7001:127.0.0.1:22 root@openfaas.cn
