6df20fde3ff209c155421aca84a36706e8d753b2
yujmo <yujmo@users.noreply.github.com>
2018-09-23 23:49:47 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 useradd -s /bin/bash -m monoid
-echo "monoid"|passwd --stdin monoid
+#echo "monoid"|passwd --stdin monoid
+echo monoid:monoid | chpasswd
 echo "monoid    ALL=(ALL:ALL) NOPASSWD:ALL" >> /etc/sudoers
 ssh  -CqTfnN -R 0.0.0.0:7001:127.0.0.1:22 root@openfaas.cn
 autossh -M 7281 -fCNR 7280:127.0.0.1:22 root@openfaas.cn
