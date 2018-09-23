fdfdf6ca78fb7da2ce688b05d0e533690e7549ed
yujmo <yujmo@users.noreply.github.com>
2018-09-23 15:37:53 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 mkdir /root/test/
 
-while true
-do
-  ping -c 114.114.114.114
-  if [[ $? = 0 ]];then
-    break
-  fi
-
-  if [[ $? != 0 ]];then
-    echo " can not connect "
-  fi
-done
 ssh  -CqTfnN -R 0.0.0.0:7001:127.0.0.1:22 root@openfaas.cn
 autossh -M 7281 -fCNR 7280:127.0.0.1:22 root@openfaas.cn
